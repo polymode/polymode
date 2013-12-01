eccb7dad3eb773a01d7730467c5c38e829a455fe
Vitalie Spinu <spinuvit@gmail.com>
2013-12-01 00:15:44 -0800

    add electric < to noweb modes

--- modes/poly-noweb.el
+++ modes/poly-noweb.el
@@ -16,5 +16,30 @@
   :group 'polymode
   :type 'object)
 
-(define-polymode poly-noweb-mode pm-config/noweb)
+(defvar poly-noweb-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map "<" 'poly-noweb-electric-<)
+    map)
+  "Poly noweb mode keymap")
+
+(define-polymode poly-noweb-mode pm-config/noweb
+  '(([?<] . poly-noweb-electric-<)))
+
+(defun ess-noweb-electric-< (arg)
+  "Auto insert noweb chunk if at bol. 
+If given an numerical argument, it simply insert `<'.
+Otherwise and if at the beginning of a line in a documentation chunk:
+insert \"<<>>=\", a closing \"@\" and a newline if necessary."
+  (interactive "P")
+  (if arg
+      (self-insert-command (if (numberp arg) arg 1))
+    (if (not (looking-back "^[ \t]*"))
+        (self-insert-command 1)
+      (insert "<<")
+      (save-excursion
+        (insert ">>=\n\n@ ")
+            (unless(looking-at "\\s *$")
+                (newline)))
+          (ess-noweb-update-chunk-vector))))
+
 (provide 'poly-noweb)
