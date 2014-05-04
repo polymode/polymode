36351cc1ebbfa9ced0561b8bf8d806aef683e131
Vitalie Spinu <spinuvit@gmail.com>
2014-05-04 01:19:33 -0700

    add polymode-configuration.el

---
+++ polymode-configuration.el
@@ -0,0 +1,24 @@
+;; Examples of polymode configuration. Choose what suits your needs and place
+;; into your .emacs file.
+
+;;; MARKDOWN
+(add-to-list 'auto-mode-alist '("\\.md" . poly-markdown-mode))
+
+
+;;; R related modes
+(add-to-list 'auto-mode-alist '("\\.Snw" . poly-noweb+r-mode))
+(add-to-list 'auto-mode-alist '("\\.Rnw" . poly-noweb+r-mode))
+(add-to-list 'auto-mode-alist '("\\.Rmd" . poly-markdown+r-mode))
+(add-to-list 'auto-mode-alist '("\\.rapport" . poly-rapport-mode))
+(add-to-list 'auto-mode-alist '("\\.Rhtml" . poly-html+r-mode))
+(add-to-list 'auto-mode-alist '("\\.Rbrew" . poly-brew+r-mode))
+(add-to-list 'auto-mode-alist '("\\.Rcpp" . poly-r+c++-mode))
+(add-to-list 'auto-mode-alist '("\\.cppR" . poly-c++r-mode))
+
+
+
+
+(provide 'polymode-configuration)
+
+
+
