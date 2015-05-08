f931a1ed4477ea0bb2911076da7759496dc98fb5
Sia. S <sia.s.saj@gmail.com>
2015-05-09 01:41:01 +1000

    Add some ERB modes

---
+++ modes/poly-erb.el
@@ -0,0 +1,42 @@
+(require 'polymode)
+
+(defcustom pm-host/coffee
+  (pm-bchunkmode "coffee" :mode 'coffee-mode)
+  "coffee host chunkmode"
+  :group 'hostmodes
+  :type 'object)
+
+(defcustom pm-host/javascript
+  (pm-bchunkmode "javascript" :mode 'js-mode)
+  "javascript host chunkmode"
+  :group 'hostmodes
+  :type 'object)
+
+(defcustom pm-poly/coffee-erb
+  (pm-polymode-one "coffee-erb"
+                   :hostmode 'pm-host/coffee
+                   :innermode 'pm-inner/erb)
+  "coffee-erb typical polymode."
+  :group 'polymodes
+  :type 'object)
+(define-polymode poly-coffee-erb-mode pm-poly/coffee-erb)
+
+(defcustom pm-poly/javascript-erb
+  (pm-polymode-one "coffee-erb"
+                   :hostmode 'pm-host/javascript
+                   :innermode 'pm-inner/erb)
+  "javascript-erb typical polymode."
+  :group 'polymodes
+  :type 'object)
+(define-polymode poly-javascript-erb-mode pm-poly/javascript-erb)
+
+(defcustom pm-poly/html-erb
+  (pm-polymode-one "html-erb"
+                   :hostmode 'pm-host/html
+                   :innermode 'pm-inner/erb)
+  "html-erb typical polymode."
+  :group 'polymodes
+  :type 'object)
+(define-polymode poly-html-erb-mode pm-poly/html-erb)
+
+(provide 'poly-erb)
