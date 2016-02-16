3ccf1bca5ac9de50e99eb0e9be9621ba2e44ed44
Vitalie Spinu <spinuvit@gmail.com>
2016-02-16 02:09:29 +0100

    Add end of string markers to auto-mode-alist regexsps

--- polymode-configuration.el
+++ polymode-configuration.el
@@ -2,21 +2,21 @@
 ;; into your .emacs file.
 
 ;;; MARKDOWN
-(add-to-list 'auto-mode-alist '("\\.md" . poly-markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md$" . poly-markdown-mode))
 
 ;;; ORG
 ;; org is not working presently
 ;; (add-to-list 'auto-mode-alist '("\\.org" . poly-org-mode))
 
 ;;; R related modes
-(add-to-list 'auto-mode-alist '("\\.Snw" . poly-noweb+r-mode))
-(add-to-list 'auto-mode-alist '("\\.Rnw" . poly-noweb+r-mode))
-(add-to-list 'auto-mode-alist '("\\.Rmd" . poly-markdown+r-mode))
-(add-to-list 'auto-mode-alist '("\\.rapport" . poly-rapport-mode))
-(add-to-list 'auto-mode-alist '("\\.Rhtml" . poly-html+r-mode))
-(add-to-list 'auto-mode-alist '("\\.Rbrew" . poly-brew+r-mode))
-(add-to-list 'auto-mode-alist '("\\.Rcpp" . poly-r+c++-mode))
-(add-to-list 'auto-mode-alist '("\\.cppR" . poly-c++r-mode))
+(add-to-list 'auto-mode-alist '("\\.Snw$" . poly-noweb+r-mode))
+(add-to-list 'auto-mode-alist '("\\.Rnw$" . poly-noweb+r-mode))
+(add-to-list 'auto-mode-alist '("\\.Rmd$" . poly-markdown+r-mode))
+(add-to-list 'auto-mode-alist '("\\.rapport$" . poly-rapport-mode))
+(add-to-list 'auto-mode-alist '("\\.Rhtml$" . poly-html+r-mode))
+(add-to-list 'auto-mode-alist '("\\.Rbrew$" . poly-brew+r-mode))
+(add-to-list 'auto-mode-alist '("\\.Rcpp$" . poly-r+c++-mode))
+(add-to-list 'auto-mode-alist '("\\.cppR$" . poly-c++r-mode))
 
 ;;; ERB modes
 (add-to-list 'auto-mode-alist '("\\.js.erb$" . poly-javascript+erb-mode))
