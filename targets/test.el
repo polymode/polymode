d3a4b4c7bd75a4916d7cf1f0a6d9e642ca92bd2a
Vitalie Spinu <spinuvit@gmail.com>
2018-08-11 21:37:19 +0200

    Add test target

---
+++ targets/test.el
@@ -0,0 +1,7 @@
+
+(setq polymode-test-dir (expand-file-name "tests/"))
+(add-to-list 'load-path polymode-test-dir)
+(dolist (f (directory-files polymode-test-dir t ".*el$"))
+  (load f))
+
+(ert-run-tests-batch-and-exit t)
