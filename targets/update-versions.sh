95f6de6af851a5b08ca74f5df323d771ad114744
Vitalie Spinu <spinuvit@gmail.com>
2018-10-10 23:11:58 +0200

    Cleanup update-versions script

--- targets/update-versions.sh
+++ targets/update-versions.sh
@@ -9,24 +9,23 @@ VERSION=$(grep Version polymode.el | sed 's/.*Version: *\(.*\) */\1/')
 
 for d in ${DIRS[@]} ;
 do
+    cd "$d"
     pkg=$(basename "$d").el
     echo "** TAGGING $pkg with v$VERSION ..."
     if git rev-parse "v$VERSION" >/dev/null 2>&1; then
         echo "** TAG EXISTS";
     else
-        cd "$d"
-        echo '** Updating version ...'
         sed -i "s/(\(poly-\?[a-z]\+\) \"[0-9.]\+\")/(\1 \"$VERSION\")/g" $pkg
         git add $pkg
         git commit -m "Version $VERSION"
         git fetch --tags
         echo '** Pushing to upstream ...'
 	    git tag "v$VERSION"
-	    # git push --set-upstream origin master
-        # git push --tags
-        cd -
+	    git push --set-upstream origin master
+        git push --tags
         echo '** DONE'
     fi
+    cd -
 done
 
 
