d98de61044c96caa026acfc3b626385e50d066d0
srnnkls <31724172+srnnkls@users.noreply.github.com>
2019-05-23 12:53:06 +0200

    Make poly-xxx-mode minor made maps instantly available for evil users

--- polymode-compat.el
+++ polymode-compat.el
@@ -325,7 +325,8 @@ changes."
           (evil-change-state old-state))))))
 
 (eval-after-load 'evil-core
-  '(add-hook 'polymode-after-switch-buffer-hook 'polymode-switch-buffer-keep-evil-state-maybe))
+  '(add-hook 'polymode-after-switch-buffer-hook 'polymode-switch-buffer-keep-evil-state-maybe)
+  '(add-hook 'polymode-init-inner-hook #'evil-normalize-keymaps))
 
 
 ;;; HL line
