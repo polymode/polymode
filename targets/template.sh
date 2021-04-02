14f9ae1fdbbb0a8845a1763dbfbecf8f28e239a0
Chris Green <greenc@fnal.gov>
2021-04-02 10:55:17 -0500

    Make update scripts work on MacOS

--- targets/template.sh
+++ targets/template.sh
@@ -3,35 +3,40 @@
 OUTDIR=$1
 MODULE=$(basename $OUTDIR)
 
+CP=cp
+T_ARG=T
+SEDI=(sed -i)
+$CP --version >/dev/null 2>&1 || { type -t gcp >/dev/null 2>&1 && CP=gcp; } || T_ARG=
+sed --version >/dev/null 2>&1 || SEDI+=('')
 POLYMODE_VERSION=$(grep Version polymode.el | sed 's/.*Version: *\(.*\) */\1/')
 
 echo "-- Copying template to $OUTDIR"
-cp  -Tfr template/ $OUTDIR
+$CP -${T_ARG}fr template/ $OUTDIR/
 
 echo "-- Replacing __MODULE__ with $MODULE"
 for f in "$OUTDIR"/* ;
 do
     if [ ! -d "$f" ]; then
-	  sed -i "s/__MODULE__/$MODULE/g" $f;
+	  "${SEDI[@]}" "s/__MODULE__/$MODULE/g" $f;
     fi
 done
 
 for f in "$OUTDIR"/targets/* ;
 do
     if [ ! -d "$f" ]; then
-	  sed -i "s/__MODULE__/$MODULE/g" $f;
+	  "${SEDI[@]}" "s/__MODULE__/$MODULE/g" $f;
     fi
 done
 
 for f in "$OUTDIR"/tests/* ;
 do
     if [ ! -d "$f" ]; then
-	  sed -i "s/__MODULE__/$MODULE/g" $f;
+	  "${SEDI[@]}" "s/__MODULE__/$MODULE/g" $f;
     fi
 done
 
 echo "-- Creating $OUTDIR/$MODULE.el"
-sed -i "s/__POLYMODE_VERSION__/$POLYMODE_VERSION/g" $OUTDIR/poly-xyz.el
+"${SEDI[@]}" "s/__POLYMODE_VERSION__/$POLYMODE_VERSION/g" $OUTDIR/poly-xyz.el
 mv -n $OUTDIR/poly-xyz.el $OUTDIR/$MODULE.el
 mv -n $OUTDIR/README-xyz.md $OUTDIR/README.md
 mv -n $OUTDIR/tests/xyz-tests.el $OUTDIR/tests/$MODULE-tests.el
