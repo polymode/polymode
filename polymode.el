6490c92fab9fd65f01899021d2655f8f3a50ff58
akater <nuclearspace@gmail.com>
2021-07-26 10:55:18 +0000

    move to -compat

--- polymode.el
+++ polymode.el
@@ -99,7 +99,6 @@ Lives on `polymode-prefix-key' in polymode buffers.")
     ["Export" polymode-export]
     ["Set Exporter" polymode-set-exporter]))
 
-(pm-add-global-localizing-advices)
 
 
 ;;; NAVIGATION
