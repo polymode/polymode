c4b6bbf63829c4d0c440c3aa420cbb3003d1f958
Stefan Monnier <monnier@iro.umontreal.ca>
2022-08-20 12:13:51 -0400

    * template/tests/xyz-tests.el: Comment `require` to fix compilation failure

--- template/tests/xyz-tests.el
+++ template/tests/xyz-tests.el
@@ -1,2 +1,13 @@
+;;; xyz-tests.el -- Tests for Polymode xyz support module  -*- lexical-binding: t; -*-
 
-(require '__MODULE__)
+;; Copyright (C) 2018-2022  Free Software Foundation, Inc.
+
+;; Uncomment and complete as appropriate.
+
+;; (require '__MODULE__)
+;;
+;; (ert-deftest __MODULE__-tests/foo-is-barred ()
+;;   (__MODULE__-blabla)
+;;   (should (__MODULE__-bar (__MODULE__-foo))))
+
+;;; xyz-tests.el ends here.
