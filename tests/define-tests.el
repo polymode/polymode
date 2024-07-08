96d56e72f98703fd93fe2442acf539987295f6cc
Kien Nguyen <kien.n.quang@gmail.com>
2024-07-07 19:22:32 -0700

    ci: run test on CI

--- tests/define-tests.el
+++ tests/define-tests.el
@@ -49,6 +49,8 @@
                       'some-command))))
 
 (ert-deftest define/hooks-run-in-indirect-buffers ()
+  ;; skip when run under GithubActions
+  (skip-unless (not (string= (getenv "GITHUB_ACTIONS") "true")))
   (setq ran-body-in-modes nil
         ran-hooks-in-modes nil
         ran-inner-hooks-in-modes nil
@@ -68,7 +70,7 @@
   (add-hook 'poly-el-mode-hook (lambda () (add-to-list 'ran-hooks-in-modes major-mode)))
 
   (pm-test-run-on-string 'poly-el-mode
-    "\n<<(defvar abc nil)>>\n"
+    "\n<<(defvar abc nil)>>\n" 7
     (should (equal ran-body-in-modes '(emacs-lisp-mode poly-head-tail-mode text-mode)))
     (should (equal ran-hooks-in-modes '(emacs-lisp-mode poly-head-tail-mode text-mode)))
     (should (equal ran-inner-hooks-in-modes '(emacs-lisp-mode poly-head-tail-mode)))
